535d619d384963f1e3bbcf157d10e26fb15b8150
Eric Green <edgeric@gmail.com>
2015-11-23 14:26:42 -0600

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+#20|20 IdentityServer3.AspNetIdentity3
+
+AspNet Identity 3 user management plugin for https://github.com/IdentityServer/IdentityServer3
---
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TwentyTwenty.IdentityServer3.AspNetIdentity3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TwentyTwenty.IdentityServer3.AspNetIdentity3")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a10c98bd-df1c-41d6-84d2-47cffa059940")]
