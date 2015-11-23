2a057e6744ff3bfab606759836df7d9482ca45b5
Eric Green <edgeric@gmail.com>
2015-11-23 14:33:46 -0600

    Added IdentityServer3 and AspNetIdentity3 dependencies and created the base class

---
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/AspNetIdentity3Plugin.cs
@@ -0,0 +1,12 @@
+﻿using IdentityServer3.Core.Services.Default;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
+{
+    public class AspNetIdentity3Plugin : UserServiceBase
+    {
+    }
+}
