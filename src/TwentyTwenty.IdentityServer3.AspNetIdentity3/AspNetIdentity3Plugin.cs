152b0eafc030125e0c4ff9aebde4090a1f1e1634
Eric Green <edgeric@gmail.com>
2015-11-23 20:00:51 -0600

    Added AuthenticateLocalAsync

--- src/TwentyTwenty.IdentityServer3.AspNetIdentity3/AspNetIdentity3Plugin.cs
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/AspNetIdentity3Plugin.cs
@@ -19,7 +19,13 @@ namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
         public AspNetIdentity3Plugin(UserManager<TUser> userManager)
         {
             _userManager = userManager;
+
+            EnableSecurityStamp = true;
         }
+
+        public string DisplayNameClaimType { get; set; }
+
+        public bool EnableSecurityStamp { get; set; }
 
         public override async Task GetProfileDataAsync(ProfileDataRequestContext context)
         {
@@ -41,6 +47,70 @@ namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
             }
 
             context.IssuedClaims = claims;
+        }
+
+        public override async Task AuthenticateLocalAsync(LocalAuthenticationContext context)
+        {
+            context.AuthenticateResult = null;
+
+            if (_userManager.SupportsUserPassword)
+            {
+                var user = await _userManager.FindByNameAsync(context.UserName);
+                if (user != null)
+                {
+                    if (_userManager.SupportsUserLockout &&
+                        await _userManager.IsLockedOutAsync(user))
+                    {
+                        return;
+                    }
+
+                    if (await _userManager.CheckPasswordAsync(user, context.Password))
+                    {
+                        if (_userManager.SupportsUserLockout)
+                        {
+                            await _userManager.ResetAccessFailedCountAsync(user);
+                        }
+
+                        var result = await PostAuthenticateLocalAsync(user, context.SignInMessage);
+                        if (result == null)
+                        {
+                            var claims = await GetClaimsForAuthenticateResult(user);
+                            result = new AuthenticateResult(user.Id.ToString(), await GetDisplayNameForAccountAsync(user.Id), claims);
+                        }
+
+                        context.AuthenticateResult = result;
+                    }
+                    else if (_userManager.SupportsUserLockout)
+                    {
+                        await _userManager.AccessFailedAsync(user);
+                    }
+                }
+            }
+        }
+
+        public override Task AuthenticateExternalAsync(ExternalAuthenticationContext context)
+        {
+            return base.AuthenticateExternalAsync(context);
+        }
+
+        public override Task IsActiveAsync(IsActiveContext context)
+        {
+            return base.IsActiveAsync(context);
+        }
+
+        public override Task PostAuthenticateAsync(PostAuthenticationContext context)
+        {
+            return base.PostAuthenticateAsync(context);
+        }
+
+        public override Task PreAuthenticateAsync(PreAuthenticationContext context)
+        {
+            return base.PreAuthenticateAsync(context);
+        }
+
+        public override Task SignOutAsync(SignOutContext context)
+        {
+            return base.SignOutAsync(context);
         }
 
         protected virtual async Task<IEnumerable<Claim>> GetClaimsFromAccount(TUser user)
@@ -88,34 +158,41 @@ namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
             return claims;
         }
 
-        public override Task AuthenticateExternalAsync(ExternalAuthenticationContext context)
+        protected virtual Task<AuthenticateResult> PostAuthenticateLocalAsync(TUser user, SignInMessage message)
         {
-            return base.AuthenticateExternalAsync(context);
+            return Task.FromResult<AuthenticateResult>(null);
         }
 
-        public override Task AuthenticateLocalAsync(LocalAuthenticationContext context)
+        protected virtual async Task<IEnumerable<Claim>> GetClaimsForAuthenticateResult(TUser user)
         {
-            return base.AuthenticateLocalAsync(context);
+            var claims = new List<Claim>();
+            if (EnableSecurityStamp && _userManager.SupportsUserSecurityStamp)
+            {
+                var stamp = await _userManager.GetSecurityStampAsync(user);
+                if (!String.IsNullOrWhiteSpace(stamp))
+                {
+                    claims.Add(new Claim("security_stamp", stamp));
+                }
+            }
+
+            return claims;
         }
 
-        public override Task IsActiveAsync(IsActiveContext context)
+        protected virtual async Task<string> GetDisplayNameForAccountAsync(string userID)
         {
-            return base.IsActiveAsync(context);
-        }
+            var user = await _userManager.FindByIdAsync(userID);
+            var claims = await GetClaimsFromAccount(user);
 
-        public override Task PostAuthenticateAsync(PostAuthenticationContext context)
-        {
-            return base.PostAuthenticateAsync(context);
-        }
+            Claim nameClaim = null;
+            if (DisplayNameClaimType != null)
+            {
+                nameClaim = claims.FirstOrDefault(x => x.Type == DisplayNameClaimType);
+            }
+            if (nameClaim == null) nameClaim = claims.FirstOrDefault(x => x.Type == Constants.ClaimTypes.Name);
+            if (nameClaim == null) nameClaim = claims.FirstOrDefault(x => x.Type == ClaimTypes.Name);
+            if (nameClaim != null) return nameClaim.Value;
 
-        public override Task PreAuthenticateAsync(PreAuthenticationContext context)
-        {
-            return base.PreAuthenticateAsync(context);
-        }
-
-        public override Task SignOutAsync(SignOutContext context)
-        {
-            return base.SignOutAsync(context);
+            return user.UserName;
         }
     }
 }
