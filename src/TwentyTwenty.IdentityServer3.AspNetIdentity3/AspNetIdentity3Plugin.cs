b5693d8b4c622c89885938982a8b1894a9ce70cb
Eric Green <edgeric@gmail.com>
2015-11-23 19:41:21 -0600

    Implemented GetProfileDataAsync

--- src/TwentyTwenty.IdentityServer3.AspNetIdentity3/AspNetIdentity3Plugin.cs
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/AspNetIdentity3Plugin.cs
@@ -3,10 +3,119 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+using IdentityServer3.Core;
+using IdentityServer3.Core.Extensions;
 
 namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
 {
-    public class AspNetIdentity3Plugin : UserServiceBase
+    public class AspNetIdentity3Plugin<TUser, TKey> : UserServiceBase
+        where TUser : class, IUser
     {
+        private readonly UserManager<TUser> _userManager;
+
+        public AspNetIdentity3Plugin(UserManager<TUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public override async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var subject = context.Subject;
+            var requestedClaimTypes = context.RequestedClaimTypes;
+
+            if (subject == null) throw new ArgumentNullException("subject");
+
+            var acct = await _userManager.FindByIdAsync(subject.GetSubjectId());
+            if (acct == null)
+            {
+                throw new ArgumentException("Invalid subject identifier");
+            }
+
+            var claims = await GetClaimsFromAccount(acct);
+            if (requestedClaimTypes != null && requestedClaimTypes.Any())
+            {
+                claims = claims.Where(x => requestedClaimTypes.Contains(x.Type));
+            }
+
+            context.IssuedClaims = claims;
+        }
+
+        protected virtual async Task<IEnumerable<Claim>> GetClaimsFromAccount(TUser user)
+        {
+            var claims = new List<Claim>{
+                new Claim(Constants.ClaimTypes.Subject, user.Id.ToString()),
+                new Claim(Constants.ClaimTypes.PreferredUserName, user.UserName),
+            };
+
+            if (_userManager.SupportsUserEmail)
+            {
+                var email = await _userManager.GetEmailAsync(user);
+                if (!String.IsNullOrWhiteSpace(email))
+                {
+                    claims.Add(new Claim(Constants.ClaimTypes.Email, email));
+                    var verified = await _userManager.IsEmailConfirmedAsync(user);
+                    claims.Add(new Claim(Constants.ClaimTypes.EmailVerified, verified ? "true" : "false"));
+                }
+            }
+
+            if (_userManager.SupportsUserPhoneNumber)
+            {
+                var phone = await _userManager.GetPhoneNumberAsync(user);
+                if (!String.IsNullOrWhiteSpace(phone))
+                {
+                    claims.Add(new Claim(Constants.ClaimTypes.PhoneNumber, phone));
+                    var verified = await _userManager.IsPhoneNumberConfirmedAsync(user);
+                    claims.Add(new Claim(Constants.ClaimTypes.PhoneNumberVerified, verified ? "true" : "false"));
+                }
+            }
+
+            if (_userManager.SupportsUserClaim)
+            {
+                claims.AddRange(await _userManager.GetClaimsAsync(user));
+            }
+
+            if (_userManager.SupportsUserRole)
+            {
+                var roleClaims =
+                    from role in await _userManager.GetRolesAsync(user)
+                    select new Claim(Constants.ClaimTypes.Role, role);
+                claims.AddRange(roleClaims);
+            }
+
+            return claims;
+        }
+
+        public override Task AuthenticateExternalAsync(ExternalAuthenticationContext context)
+        {
+            return base.AuthenticateExternalAsync(context);
+        }
+
+        public override Task AuthenticateLocalAsync(LocalAuthenticationContext context)
+        {
+            return base.AuthenticateLocalAsync(context);
+        }
+
+        public override Task IsActiveAsync(IsActiveContext context)
+        {
+            return base.IsActiveAsync(context);
+        }
+
+        public override Task PostAuthenticateAsync(PostAuthenticationContext context)
+        {
+            return base.PostAuthenticateAsync(context);
+        }
+
+        public override Task PreAuthenticateAsync(PreAuthenticationContext context)
+        {
+            return base.PreAuthenticateAsync(context);
+        }
+
+        public override Task SignOutAsync(SignOutContext context)
+        {
+            return base.SignOutAsync(context);
+        }
     }
 }
---
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/IUser.cs
@@ -0,0 +1,9 @@
+﻿namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
+{
+    public interface IUser
+    {
+        string Id { get; set; }
+
+        string UserName { get; set; }
+    }
+}
