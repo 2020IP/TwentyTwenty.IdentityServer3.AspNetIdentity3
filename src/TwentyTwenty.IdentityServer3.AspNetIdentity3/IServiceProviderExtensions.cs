29dbef34ffc516d3589776647609969989a8a3c5
Eric Green <edgeric@gmail.com>
2015-12-18 12:01:20 -0600

    Added service registration

---
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/IServiceProviderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using IdentityServer3.Core.Services;
+using System;
+
+namespace TwentyTwenty.IdentityServer3.AspNetIdentity3
+{
+    internal static class IServiceProviderExtensions
+    {
+        public static void ThrowIfUserServicesNotRegistered(this IServiceProvider services)
+        {
+            if (services.GetService(typeof(IUserService)) == null)
+            {
+                throw new InvalidOperationException("Must add UserService to DependencyInjection container before calling RegisterUserServices");
+            }
+        }
+    }
+}
---
+++ src/TwentyTwenty.IdentityServer3.AspNetIdentity3/IdentityServerServiceFactoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using TwentyTwenty.IdentityServer3.AspNetIdentity3;
+
+namespace TwentyTwenty.Identity.Extensions
+{
+    public static class IdentityServerServiceFactoryExtensions
+    {
+        public static IdentityServerServiceFactory RegisterUserServices(this IdentityServerServiceFactory factory, IServiceProvider services)
+        {
+            if (factory == null) throw new ArgumentNullException("factory");
+            if (services == null) throw new ArgumentNullException("services");
+            services.ThrowIfUserServicesNotRegistered();
+
+            factory.UserService = services.GetRegistration<IUserService>();
+
+            return factory;
+        }
+
+        private static Registration<T> GetRegistration<T>(this IServiceProvider services) where T : class
+        {
+            return new Registration<T>(resolver => services.GetRequiredService<T>());
+        }
+    }
+}
